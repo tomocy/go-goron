1c0a0d47a44f13babac1e55e975305468939a66b
tomocy <tomocy.dev@gmail.com>
2018-05-08 10:20:48 +0000

    [mv] storages.memory to memory.memory

---
+++ session/storages/memory/memory.go
@@ -0,0 +1,32 @@
+package memory
+
+import (
+	"errors"
+
+	"github.com/tomocy/goron/session"
+)
+
+var sessions = make(map[string]session.Session)
+
+type memory struct {
+}
+
+func New() *memory {
+	return &memory{}
+}
+
+func (m *memory) InitSession(sessionID string) session.Session {
+	dat := make(map[string]interface{})
+	sessions[sessionID] = session.New(sessionID, dat)
+
+	return sessions[sessionID]
+}
+
+func (m *memory) GetSession(sessionID string) (session.Session, error) {
+	session, ok := sessions[sessionID]
+	if !ok {
+		return nil, errors.New("Session not found")
+	}
+
+	return session, nil
+}

7d3f19a25c4970f6c6705279aedad753c7f95f48
tomocy <tomocy.dev@gmail.com>
2018-05-08 10:18:54 +0000

    [mv] session.managet to manager.manager

---
+++ session/manager/manager.go
@@ -0,0 +1,42 @@
+package manager
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/tomocy/goron/session"
+	"github.com/tomocy/goron/session/storages"
+)
+
+type Manager interface {
+	GetSession(string) (session.Session, error)
+	CreateSession() session.Session
+}
+
+type manager struct {
+	storage storages.Storage
+}
+
+func New(storageName string) (Manager, error) {
+	storage, err := storages.Get(storageName)
+	if err != nil {
+		return nil, fmt.Errorf("Storage not found: %s", storageName)
+	}
+
+	return &manager{storage: storage}, nil
+}
+
+func (m *manager) GetSession(sessionID string) (session.Session, error) {
+	session, err := m.storage.GetSession(sessionID)
+	if err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+func (m *manager) CreateSession() session.Session {
+	sessionID := uuid.New().String()
+
+	return m.storage.InitSession(sessionID)
+}

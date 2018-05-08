f3ab7cfa0733e0e29baff464951d495be1a073bb
tomocy <tomocy.dev@gmail.com>
2018-05-08 10:16:11 +0000

    [mod] change pakcage name

--- session/session.go
+++ session/session.go
@@ -4,56 +4,31 @@ type Session interface {
 	Set(key string, value interface{})
 	Get(key string) interface{}
 	// Delete(key string) error
-	// ID() string
+	ID() string
 }
 
 type session struct {
+	id  string
+	dat map[string]interface{}
 }
 
-func New() Session {
-	return &session{}
+func New(id string, dat map[string]interface{}) Session {
+	return &session{id: id, dat: dat}
 }
-
-var sessions map[string]interface{}
 
 func (s *session) Set(key string, value interface{}) {
-	sessions[key] = value
+	s.dat[key] = value
 }
 
 func (s *session) Get(key string) interface{} {
-	if v, ok := sessions[key]; ok {
-		return v
+	v, ok := s.dat[key]
+	if !ok {
+		return nil
 	}
 
-	return nil
+	return v
 }
 
-func init() {
-	sessions = make(map[string]interface{})
+func (s *session) ID() string {
+	return s.id
 }
-
-// type session struct {
-// 	id        Id
-// 	dat       map[string]interface{}
-// 	expiresAt time.Time
-// }
-
-// type Id string
-
-// const expiresIn time.Duration = (3 * time.Minute)
-
-// func (s *session) Set(key string, value interface{}) error {
-// 	return nil
-// }
-
-// func (s *session) Get(key string) (interface{}, error) {
-// 	return nil, nil
-// }
-
-// func (s *session) Delete(key string) error {
-// 	return nil
-// }
-
-// func (s *session) ID() Id {
-// 	return s.id
-// }

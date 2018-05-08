8544aa36ebff325afe6979d584cba758b900297d
tomocy <tomocy.dev@gmail.com>
2018-05-08 10:19:37 +0000

    [mv] session.storage to storages.storage

--- session/storages/storage.go
+++ session/storages/storage.go
@@ -1,15 +1,21 @@
 package storages
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/tomocy/goron/session"
+	"github.com/tomocy/goron/session/storages/memory"
+)
 
 type Storage interface {
-	InitSession()
+	InitSession(sessionID string) session.Session
+	GetSession(sessionID string) (session.Session, error)
 }
 
 func Get(storage string) (Storage, error) {
 	switch storage {
 	case "memory":
-		return &memory{}, nil
+		return memory.New(), nil
 	default:
 		return nil, errors.New("Not found")
 	}

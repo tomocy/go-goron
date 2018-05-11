9f27e3a2133df032c7c1f485ef8ce0edbf4066e0
tomocy <tomocy.dev@gmail.com>
2018-05-11 16:00:23 +0000

    memory reimplements Storage interface

--- session/storages/memory/memory.go
+++ session/storages/memory/memory.go
@@ -31,11 +31,6 @@ func (m *memory) GetSession(sessionID string) (session.Session, error) {
 	return session, nil
 }
 
-func (m *memory) DestroySession(sessionID string) error {
-	if _, ok := sessions[sessionID]; ok {
-		delete(sessions, sessionID)
-		return nil
-	}
-
-	return errors.New("Session not found: could not delete the session")
+func (m *memory) SetSession(sessionID string, dat map[string]interface{}) {
+	sessions[sessionID] = session.New(sessionID, dat)
 }

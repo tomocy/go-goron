4f0c83a78bf3a8f8422b997b1c7afb52d74e145b
tomocy <tomocy.dev@gmail.com>
2018-05-14 10:19:43 +0000

    add mutex to session

--- session/session.go
+++ session/session.go
@@ -1,15 +1,17 @@
 package session
 
+import "sync"
+
 type Session interface {
 	Set(key string, value interface{})
 	Get(key string) interface{}
-	// Delete(key string) error
 	ID() string
 }
 
 type session struct {
 	id  string
 	dat map[string]interface{}
+	mu  sync.Mutex
 }
 
 func New(id string, dat map[string]interface{}) Session {
@@ -17,10 +19,16 @@ func New(id string, dat map[string]interface{}) Session {
 }
 
 func (s *session) Set(key string, value interface{}) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.dat[key] = value
 }
 
 func (s *session) Get(key string) interface{} {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	v, ok := s.dat[key]
 	if !ok {
 		return nil

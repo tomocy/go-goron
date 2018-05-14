90a90d7b67edab59e5e414f071d7486e778ced48
tomocy <tomocy.dev@gmail.com>
2018-05-14 10:04:47 +0000

    moved routing to route package

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/tomocy/goron/route"
 )
 
 var r route.Route
 
 func main() {
-	r.Listen()
-
-	fmt.Println("Listeing :8080")
-	http.ListenAndServe(":8080", nil)
+	r.ListenAndServe()
 }
 
 func init() {

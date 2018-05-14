008226b43ddcb110ba1d57898ab38b8b5915c66d
tomocy <tomocy.dev@gmail.com>
2018-05-14 10:02:16 +0000

    moved http.ListenAndServe to route package

--- route/route.go
+++ route/route.go
@@ -1,23 +1,41 @@
 package route
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/tomocy/goron/handlers/count"
 )
 
 type Route interface {
-	Listen()
+	ListenAndServe()
+
+	serve()
 }
 
 type route struct {
+	stopCh chan os.Signal
 }
 
 func New() Route {
-	return &route{}
+	stopCh := make(chan os.Signal, 1)
+	return &route{stopCh: stopCh}
 }
 
-func (r *route) Listen() {
+func (r *route) ListenAndServe() {
 	// Write root
 	http.HandleFunc("/count", count.Index)
+
+	// And serve
+	r.serve()
+}
+
+func (r *route) serve() {
+	fmt.Println("Listeing :8080")
+	http.ListenAndServe(":8080", nil)
+	signal.Notify(r.stopCh, syscall.SIGINT, syscall.SIGTERM)
+	<-r.stopCh
 }

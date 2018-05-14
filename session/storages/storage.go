e199a693b9ee2179fdbc6e7d8ae89a0a62235d64
tomocy <tomocy.dev@gmail.com>
2018-05-14 12:30:20 +0000

    add SetSession method

--- session/storages/storage.go
+++ session/storages/storage.go
@@ -4,18 +4,22 @@ import (
 	"errors"
 
 	"github.com/tomocy/goron/session"
+	"github.com/tomocy/goron/session/storages/file"
 	"github.com/tomocy/goron/session/storages/memory"
 )
 
 type Storage interface {
 	InitSession(sessionID string) session.Session
 	GetSession(sessionID string) (session.Session, error)
+	SetSession(session session.Session)
 }
 
 func Get(storage string) (Storage, error) {
 	switch storage {
 	case "memory":
 		return memory.New(), nil
+	case "file":
+		return file.New(), nil
 	default:
 		return nil, errors.New("Not found")
 	}

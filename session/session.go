3e68fb782f1e56fb9ce455905513ddc28a165db6
tomocy <tomocy.dev@gmail.com>
2018-05-14 12:29:38 +0000

    change dat type to map[string]string

--- session/session.go
+++ session/session.go
@@ -3,35 +3,36 @@ package session
 import "sync"
 
 type Session interface {
-	Set(key string, value interface{})
-	Get(key string) interface{}
+	Set(k string, v string)
+	Get(k string) string
 	ID() string
+	Data() map[string]string
 }
 
 type session struct {
 	id  string
-	dat map[string]interface{}
+	dat map[string]string
 	mu  sync.Mutex
 }
 
-func New(id string, dat map[string]interface{}) Session {
+func New(id string, dat map[string]string) Session {
 	return &session{id: id, dat: dat}
 }
 
-func (s *session) Set(key string, value interface{}) {
+func (s *session) Set(k string, v string) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	s.dat[key] = value
+	s.dat[k] = v
 }
 
-func (s *session) Get(key string) interface{} {
+func (s *session) Get(k string) string {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	v, ok := s.dat[key]
+	v, ok := s.dat[k]
 	if !ok {
-		return nil
+		return ""
 	}
 
 	return v
@@ -39,4 +40,8 @@ func (s *session) Get(key string) interface{} {
 
 func (s *session) ID() string {
 	return s.id
+}
+
+func (s *session) Data() map[string]string {
+	return s.dat
 }

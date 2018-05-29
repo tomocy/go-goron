8817ee991324de7b9ad1081444c6d04f97a70d08
tomocy <tomocy.dev@gmail.com>
2018-05-29 09:14:38 +0000

    modified make []byte and came to make []byte with size 0 and 10 buffers

--- tests/storages/file/helper.go
+++ tests/storages/file/helper.go
@@ -5,3 +5,11 @@ import "github.com/google/uuid"
 func generateSessionID() string {
 	return uuid.New().String()
 }
+
+func getSessionFilePath(sessionID string) string {
+	b := make([]byte, 0, 10)
+	b = append(b, "storage/sessions/"...)
+	b = append(b, sessionID...)
+
+	return string(b)
+}

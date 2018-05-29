3dcffe705c390771d02581ea01b5bcd8e746e39f
tomocy <tomocy.dev@gmail.com>
2018-05-29 08:13:52 +0000

    came to use tlog package

--- tests/storages/file/file_test.go
+++ tests/storages/file/file_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/tomocy/goron/log/tlog"
 	"github.com/tomocy/goron/session/storages/file"
 )
 
@@ -21,7 +22,7 @@ func TestInitSession(t *testing.T) {
 	// functino to be tested
 	sess1 := f.InitSession(sess1ID)
 	if sess1ID != sess1.ID() {
-		t.Errorf("could not init session with passed id\nwanted: %s\nhad: %s", sess1ID, sess1.ID())
+		t.Error(tlog.GetWantedHad("could not init session with passed id", sess1ID, sess1.ID()))
 	}
 
 	sess2, err := f.GetSession(sess1ID)
@@ -30,10 +31,10 @@ func TestInitSession(t *testing.T) {
 	}
 
 	if sess1.ID() != sess2.ID() {
-		t.Errorf("session id not same\nwanted: %s\nhad: %s", sess1.ID(), sess2.ID())
+		t.Error(tlog.GetWantedHad("session id not same", sess1.ID(), sess2.ID()))
 	}
 	if !reflect.DeepEqual(sess1.Data(), sess2.Data()) {
-		t.Errorf("data in session not same\nwanted: %#v\nhad: %#v", sess1.Data(), sess2.Data())
+		t.Error(tlog.GetWantedHad("data in session not same", sess1.Data(), sess2.Data()))
 	}
 	if !sess1.ExpiresAt().Equal(sess2.ExpiresAt()) {
 		t.Error("expires of session not same")
